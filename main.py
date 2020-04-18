e35bc3189b0a1ad88d911a50afeebf7822774612
Tyler Bateman <batemat2@wwu.edu>
2020-04-17 20:37:48 -0700

    Built backtrace

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import sys
 from sys import argv
+from bt_constants import *
 
 def distance(target, source, insertcost, deletecost, replacecost):
     n = len(target) + 1
     m = len(source) + 1
     # set up dist and initialize values
     dist = [ [0 for j in range(m)] for i in range(n)  ]
+    bt = [[0 for j in range(m)] for i in range(n) ]
     for i in range(1, n):
         dist[i][0] = dist[i-1][0] + insertcost
     for j in range(1, m):
@@ -22,7 +24,12 @@ def distance(target, source, insertcost, deletecost, replacecost):
                 add = replacecost
             substcost = add + dist[i-1][j-1]
             dist[i][j] = min(inscost, delcost, substcost)
-            
+            if dist[i][j] == substcost:
+                bt[i][j] = SUBSTITUTE
+            elif dist[i][j] == inscost:
+                bt[i][j] = INSERT
+            else:
+                bt[i][j] = DELETE
     # return min edit distance
     return dist[n-1][m-1]
 
