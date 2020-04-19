3e42222fac0f8497717049b92971ac462b0db996
Tyler Bateman <batemat2@wwu.edu>
2020-04-18 20:13:00 -0700

    Complete

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ def distance(target, source, insertcost, deletecost, replacecost):
     bt = [[0 for j in range(m)] for i in range(n) ]
     for i in range(1, n):
         dist[i][0] = dist[i-1][0] + insertcost
+        bt[i][0] = LEFT 
     for j in range(1, m):
         dist[0][j] = dist[0][j-1] + deletecost
+        bt[0][j] = DOWN 
 
     #align source and target strings
     for j in range(1,m):
@@ -25,16 +27,45 @@ def distance(target, source, insertcost, deletecost, replacecost):
             substcost = add + dist[i-1][j-1]
             dist[i][j] = min(inscost, delcost, substcost)
             if dist[i][j] == substcost:
-                bt[i][j] = SUBSTITUTE
+                bt[i][j] = DIAG 
             elif dist[i][j] == inscost:
-                bt[i][j] = INSERT
+                bt[i][j] = LEFT 
             else:
-                bt[i][j] = DELETE
+                bt[i][j] = DOWN
+
     # return min edit distance
-    return dist[n-1][m-1]
+    return (dist[n-1][m-1], bt)
+
+def generate_alignment(target, source, bt):
+    t_format = ""
+    connections = ""
+    s_format = ""
+    n = len(target) 
+    m = len(source)
+    while m > 0 or n > 0:
+        if bt[n][m] == DIAG:
+            t_format = target[n - 1] + " " + t_format
+            s_format = source[m - 1] + " " + s_format
+            connections = "| " + connections if target[n - 1] == source[m - 1] else "  " + connections
+            n -= 1
+            m -= 1
+        elif bt[n][m] == LEFT:
+            t_format = target[n - 1] + " " + t_format
+            s_format = "_ " + s_format
+            connections = "  " + connections
+            n -= 1
+        else:
+            t_format = "_ " + t_format
+            s_format = source[m - 1] + " " + s_format 
+            connections = "  " + connections
+            m -= 1
+    return (t_format, connections, s_format)
+
 
 
 if __name__ == "__main__":
     if len(argv) > 2:
-        print "levenshtein distance = {}".format(distance(argv[1], argv[2], 1, 1, 2))
-
+        dist_data = distance(argv[1], argv[2], 1, 1, 2) 
+        print "levenshtein distance = {}".format(dist_data[0])
+        for s in generate_alignment(argv[1], argv[2], dist_data[1]):
+            print s
